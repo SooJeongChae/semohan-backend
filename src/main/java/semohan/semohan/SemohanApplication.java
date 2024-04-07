30b85899e6beb1e7a316cfb06382403a0b136b90
Songhee Lee <clasonghee@gmail.com>
2024-04-07 21:24:54 +0900

    init

---
+++ src/main/java/semohan/semohan/SemohanApplication.java
@@ -0,0 +1,13 @@
+package semohan.semohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemohanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemohanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/semohan/semohan/SemohanApplicationTests.java
@@ -0,0 +1,13 @@
+package semohan.semohan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemohanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
