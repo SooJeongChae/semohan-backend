d593f489356d425308010af7c2593339bc39f5af
sjchae <soojch12@gmail.com>
2024-06-01 04:15:31 +0900

    feat: 로그인 여부 확인 기능 구현

---
+++ src/main/java/semohan/semohan/global/auth/LoginConfig.java
@@ -0,0 +1,21 @@
+package semohan.semohan.global.auth;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class LoginConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns(
+                        "/member/**",
+                        "/auth/sign-out",
+                        "/menu/pin"
+                        // 추가적인 로그인 필요한 패턴들
+                );
+    }
+}
